2afb16e54bdb51c8a7cec72df66aefe080636748
YH <yan.huang@uth.tmc.edu>
2024-03-05 16:24:22 -0600

    tel function update

--- telii-rocket/src/api/eeg_query_api.rs
+++ telii-rocket/src/api/eeg_query_api.rs
@@ -1,4 +1,4 @@
-use std::result;
+use std::{result, vec};
 
 use crate::{models::event::Event, database::mongodb::EegMongoRepo};
 use mongodb::{bson::{doc, Document,Bson}, Collection, results::{self, InsertOneResult}};
@@ -45,7 +45,7 @@ impl TelExp {
 pub fn eeg_allen_query(db: &State<EegMongoRepo>, relation: &str, event_id_list1: &str, event_id_list2: &str) -> Result<Json<Vec<Document>>, Status> {
 	// valid operations: before, after, overlap, contain, start, end
 	let relation = relation.to_lowercase();
-	if relation != "before" && relation != "after" && relation != "overlap" && relation != "contain" && relation != "start" && relation != "end" {
+	if relation != "before" && relation != "overlap" && relation != "contain" && relation != "start" && relation != "end" {
 		return Err(Status::NotFound);
 	}
 	let event_id_list1: Vec<i32> = event_id_list1.split(',')
@@ -66,33 +66,116 @@ pub fn eeg_allen_query(db: &State<EegMongoRepo>, relation: &str, event_id_list1:
 	let ts = hashmap!{
 		"t" => "e1",
 	};
-	let exps = vec![
-		TelExp::init("box_t_phi", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None)
-	];
+	let mut exps = Vec::new();
+	if relation == "before"{
+		exps = vec![
+			TelExp::init("box_t_phi", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_t_neg_phi", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_neg_phi_t", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_neg_phi_t", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None)
+		];
+	}else if relation == "contain"{
+		exps = vec![
+			TelExp::init("box_t_phi", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_t_neg_phi", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_phi_t", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_neg_phi_t", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None)
+		];
+	}else if relation == "start"{
+		exps = vec![
+			TelExp::init("box_t_phi", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_t_phi", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_phi_t", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_neg_phi_t", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None)
+		];
+	}else if relation == "end"{
+		exps = vec![
+			TelExp::init("box_t_phi", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_t_neg_phi", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_phi_t", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_phi_t", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None)
+		];
+	}else if relation == "overlap"{
+		exps = vec![
+			TelExp::init("box_t_phi", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_t_neg_phi", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("box_phi_t", "t", "e2", Some(vec!["e1","e2"]), Some(0.0), None, None),
+			TelExp::init("diamond_neg_phi_t", "t", "e1", Some(vec!["e1","e2"]), Some(0.0), None, None)
+		];
+	}
+
 
 	let pipeline = construct_query(events, ts, exps);
+	for _step in pipeline.clone(){
+		println!("{:?}", _step);
+	}
+	let mut results = Vec::new();
 
-	let results = Vec::new();
+	let mut cursor = db.timeline_col.aggregate(pipeline, None).unwrap();
+  while let Some(result) = cursor.next() {
+    match result {
+      Ok(document) => {
+        // println!("document: {:?}", document);
+        results.push(document);
+      }
+      Err(e) => {
+        println!("Error getting result");
+        return Err(Status::InternalServerError);
+      }
+    }
+  }
+
 	match results.len() {
-	_ => Ok(Json(results)),
-	// 0 => Err(Status::NotFound),
+		_ => Ok(Json(results)),
+		// 0 => Err(Status::NotFound),
 	}
 }
 
 pub fn construct_query(events: HashMap<&str,Vec<i32>>,ts:HashMap<&str,&str>,exps:Vec<TelExp>) -> Vec<Document> {
-	let mongo_stmt = Vec::new();
-
 	// get all event ids from values of events
-	let mut event_ids: HashSet<i32>= HashSet::new();
-	for (_k,v) in events.iter() {
-		event_ids.extend(v);
+	let mut event_ids: HashSet<i32> = HashSet::new();
+	let mut group_stmt = doc!{"_id": "$subjectid"};
+	let mut filter_none_time_stmt = doc!{ "_id":1};
+	let mut filter = Vec::<Document>::new();
+
+	for (_k,_v) in events.iter() {
+		let event_name = _k.to_string(); // Clone the value of event_name
+
+		event_ids.extend(_v);
+		group_stmt.insert(*_k, doc!{ "$addToSet": { "$cond": [ { "$in": [ "$e", _v ] }, "$times", None::<i32> ] } });
+		filter_none_time_stmt.insert(event_name.clone(), doc!{"$setDifference": [ format!("${}", event_name), [None::<i32>]]}); 
+		filter.push(doc!{"$gt": [ {"$size": format!("${}", event_name)}, 0]});
 	}
-	// get a set of unique event ids
-	
+	let event_ids: Vec<i32> = event_ids.into_iter().collect();
 
-
+	// get tel conditions
 	let tel_cond_stmt = construct_tel_cond(exps);
-	print!("{:?}", tel_cond_stmt);
+	// print!("{:?}", tel_cond_stmt);
+
+	let mut mongo_stmt = vec![
+		doc!{"$match": {"e": {"$in": event_ids}}},
+		doc!{"$project": {"_id": 0, "subjectid": 1, "e": 1, "times": 1}},
+		doc!{"$unwind": "$times"},
+		doc!{"$group": group_stmt},
+		doc!{"$project": filter_none_time_stmt},
+		doc!{"$match": { "$expr": { "$and": filter } }}
+	];
+	let mut project_stmt = doc!{"_id": 1};
+	for (_k,_v) in ts.iter() {
+		project_stmt.insert(_k.to_string(), format!("${}", _v));
+	}
+	for _k in events.keys() {
+		mongo_stmt.push(doc!{"$unwind": format!("${}", _k)});
+		project_stmt.insert(format!("min_{}", _k),doc!{"$arrayElemAt": [format!("${}", _k), 0]});
+		project_stmt.insert(format!("max_{}", _k),doc!{"$arrayElemAt": [format!("${}", _k), -1]});
+	}
+	mongo_stmt.push(doc!{"$project": project_stmt});
+	for _k in ts.keys() {
+		mongo_stmt.push( doc!{"$unwind": format!("${}", _k)} );
+	}
+	mongo_stmt.push(doc!{"$addFields": {"tel_cond": tel_cond_stmt}});
+	mongo_stmt.push(doc!{"$match": {"tel_cond": true}});
+
 
 	return mongo_stmt;
 
@@ -104,8 +187,20 @@ pub fn construct_tel_cond(exps:Vec<TelExp>) -> Document {
 		let mut mongo_exp = doc!{};
 		if exp.operator == "box_t_phi" {
 			mongo_exp = box_t_phi(exp);
-		} else {
-			mongo_exp = doc! {};
+		} else if exp.operator == "box_t_neg_phi" {
+			mongo_exp = box_t_neg_phi(exp);
+		} else if exp.operator == "box_phi_t" {
+			mongo_exp = box_phi_t(exp);
+		} else if exp.operator == "box_neg_phi_t" {
+			mongo_exp = box_neg_phi_t(exp);
+		} else if exp.operator == "diamond_t_phi" {
+			mongo_exp = diamond_t_phi(exp);
+		} else if exp.operator == "diamond_t_neg_phi" {
+			mongo_exp = diamond_t_neg_phi(exp);
+		} else if exp.operator == "diamond_phi_t" {
+			mongo_exp = diamond_phi_t(exp);
+		} else if exp.operator == "diamond_neg_phi_t" {
+			mongo_exp = diamond_neg_phi_t(exp);
 		}
 		
 		and_stmt.push(mongo_exp);
